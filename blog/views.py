7f9ce5c20d15cfc944a5881564fdd073831ddd35
TheAbhijeet <38559396+TheAbhijeet@users.noreply.github.com>
2019-05-19 17:26:18 +0530

    Filters added

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
 
 class PostDetail(generic.DetailView):
