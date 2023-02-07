01f351eb0ec3003da164df09865db4210fafb9d7
Mohamed Talaat <102768699+MohamedTalaat-Coder@users.noreply.github.com>
2023-02-07 14:15:15 +0200

    add all posts url

--- src/blog/views.py
+++ src/blog/views.py
@@ -16,25 +16,23 @@ class PostList(generic.ListView):
 #     template_name = 'post_detail.html'
 
 class PostTag(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
 
     def get(self, request, *args, **kwargs):
-        print(args, kwargs)
-        post_list = Post.objects.filter(tag=kwargs['tag'])
-        return render(request, self.template_name, context={'post_list': post_list})
+        posts = Post.objects.filter(tag=kwargs['tag'])
+        return render(request, self.template_name, {"post_list": posts})
 
 
 def post_detail(request, slug):
     template_name = "post_detail.html"
     post = get_object_or_404(Post, slug=slug)
+    print("post objects-->", post)
     comments = post.comments.filter(active=True).order_by("-created_on")
     new_comment = None
     # Comment posted
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-
             # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
             # Assign the current post to the comment
@@ -54,3 +52,8 @@ def post_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+
+class AllPosts(generic.ListView):
+    template_name = "index.html"
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    paginate_by = 3
--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 import debug_toolbar
-
 from blog.sitemaps import PostSitemap
 
 sitemaps = {
